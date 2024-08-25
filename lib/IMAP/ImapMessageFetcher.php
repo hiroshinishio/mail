7abda4863bc5e3d5c7bbdd09819427d90957b503
SebastianKrupinski <krupinskis05@gmail.com>
2024-08-25 08:59:50 -0400

    fix: retrieve body structure and process parts on mailbox sync
    Signed-off-by: SebastianKrupinski <krupinskis05@gmail.com>

--- lib/IMAP/ImapMessageFetcher.php
+++ lib/IMAP/ImapMessageFetcher.php
@@ -203,7 +203,32 @@ class ImapMessageFetcher {
 					'forcemime' => true,
 				]);
 			}
+		} elseif (is_null($fetch)) {
+			// Reuse given query or construct a new minimal one
+			$query = new Horde_Imap_Client_Fetch_Query();
+			$query->envelope();
+			$query->structure();
+			$query->flags();
+			$query->imapDate();
+			$query->headerText([
+				'peek' => true,
+			]);
 
+			$result = $this->client->fetch($this->mailbox, $query, ['ids' => $ids]);
+			$fetch = $result[$this->uid];
+			if (is_null($fetch)) {
+				throw new DoesNotExistException("This email ($this->uid) can't be found. Probably it was deleted from the server recently. Please reload.");
+			}
+		}
+
+		// determine if message structure was already extracted
+		if (!isset($structure)) {
+			// extract message structure
+			$structure = $fetch->getStructure();
+		}
+		// determine if we finally have a message structure
+		// and process the message parts if structure exists
+		if ($structure !== null) {
 			// debugging below
 			$structure_type = $structure->getPrimaryType();
 			if ($structure_type === 'multipart') {
@@ -216,21 +241,6 @@ class ImapMessageFetcher {
 				if (!is_null($bodyPartId)) {
 					$this->getPart($structure[$bodyPartId], $bodyPartId, $isEncrypted || $isSigned);
 				}
-			}
-		} elseif (is_null($fetch)) {
-			// Reuse given query or construct a new minimal one
-			$query = new Horde_Imap_Client_Fetch_Query();
-			$query->envelope();
-			$query->flags();
-			$query->imapDate();
-			$query->headerText([
-				'peek' => true,
-			]);
-
-			$result = $this->client->fetch($this->mailbox, $query, ['ids' => $ids]);
-			$fetch = $result[$this->uid];
-			if (is_null($fetch)) {
-				throw new DoesNotExistException("This email ($this->uid) can't be found. Probably it was deleted from the server recently. Please reload.");
 			}
 		}
 
